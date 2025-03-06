efcbdb825b39e96f0dc78b039249d656ca42de03
amirmx2905 <amir.flores.cardona@gmail.com>
2025-03-06 17:23:31 -0600

    ADDED create and delete note views and ADDED them to the paths, also made the migrations of the api folder

--- backend/api/views.py
+++ backend/api/views.py
@@ -6,7 +6,28 @@ from rest_framework.permissions import IsAuthenticated, AllowAny
 from .models import Note
 
 
-# Create your views here.
+class noteListCreate(generics.ListCreateAPIView):
+    serializer_class = NoteSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        user = self.request.user
+        return Note.objects.filter(author=user)
+    
+    def perform_create(self, serializer):
+        serializer.save(author=self.request.user)
+        if serializer.is_valid():
+            serializer.save(author=self.request.user)
+        else:
+            print(serializer.errors)
+
+class noteDelete(generics.DestroyAPIView):
+    serializer_class = NoteSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        user = self.request.user
+        return Note.objects.filter(author=user)
 
 class createUserView(generics.CreateAPIView):
     queryset = User.objects.all()
